e209a6a8dd8a798be0e96ed7c803331add8e8514
mikelghedina <mikel.ghedina@gmail.com>
2021-03-30 13:54:41 +0200

    icon navbar included

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Nav from 'react-bootstrap/Navbar'
 import Navbar from "react-bootstrap/Navbar";
 import NavLink from "react-bootstrap/NavLink";
+import logo from '../logo.png'
 
 const navbar  = () =>{
 
     return(
         <React.Fragment>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="/">Flat Find</Navbar.Brand>
+                <Navbar.Brand href="/">
+                    <img src={logo} alt="logo" width="60px"/>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
