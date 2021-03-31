f8f54a7dd496a30b772dd92069cb8dd045b31010
mikelghedina <mikel.ghedina@gmail.com>
2021-03-31 13:31:59 +0200

    Link className Empezar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,19 @@
 import React from "react";
 import './Home.css';
+import {Link} from "react-router-dom";
 // import Research from "./components/Research/Research";
-import logo from '../logo.png'
+
 
 const Home =()=>(
 
     <React.Fragment>
     {/* <img src={logo}></img> */}
     <div className='background'>
-    <div className='linia1'></div>
-        <div className='introFlatFind'>Flat Find te permite encontrar el precio adecuado con las caracteristicas de tu piso ideal en Barcelona.
-        Te recomienda los barrios que se adecuan al precio y a tus necesidades.</div>
-    </div>
-    <button path='/research' className='consulta'>Empezar</button>
+        <div className='linia1'></div>
+            <div className='introFlatFind'>Flat Find te permite encontrar el precio adecuado con las caracteristicas de tu piso ideal en Barcelona.
+            Te recomienda los barrios que se adecuan al precio y a tus necesidades.</div>
+        </div>
+        <Link to='/research' className='consulta'>Empezar</Link>
     </React.Fragment>
 
 
