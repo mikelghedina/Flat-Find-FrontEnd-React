20ce22f97a080d941ddeb0696f8c5cbcf02de2e1
gemmarevsan <69312176+gemmarevsan@users.noreply.github.com>
2021-03-31 12:39:05 +0200

    css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
-
+import './Home.css';
+// import Research from "./components/Research/Research";
+import logo from '../logo.png'
 
 const Home =()=>(
-    <div>
-        <h1>Home</h1>
+
+    <React.Fragment>
+    {/* <img src={logo}></img> */}
+    <div className='background'>
+    <div className='linia1'></div>
+        <div className='introFlatFind'>Flat Find te permite encontrar el precio adecuado con las caracteristicas de tu piso ideal en Barcelona.
+        Te recomienda los barrios que se adecuan al precio y a tus necesidades.</div>
     </div>
+    <button path='/research' className='consulta'>Empezar</button>
+    </React.Fragment>
+
+
+
+
+
+
 );
 
 export default Home;
