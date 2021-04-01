fd70368f5fa95467f12eaeee3bb89cc90a3acfbb
gemmarevsan <69312176+gemmarevsan@users.noreply.github.com>
2021-04-01 12:40:23 +0200

    select

--- src/components/Research/Research.js
+++ src/components/Research/Research.js
@@ -9,9 +9,13 @@ class Research extends Component {
 
 
     state={
-        district_name:['Ciutat Vella', 'Eixample'],
+        district_names:["Ciutat Vella","Eixample", "Gracia","Horta" , "Les Corts" , "Nou Barris" , "Sant Andreu", "Sant Marti","Sants Montjuic", "Sarria"],
+        district_name:'',
         sup:'',
+        sup_list:[40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200],
+        bath_list:[1,2,3],
         baths:'',
+        room_list:[1,2,3,4,5],
         rooms:''
     }
 
@@ -37,10 +41,24 @@ class Research extends Component {
                                 ¿En que distrito quieres vivir?
                             </Col>                                
                             <Col>
-                                <Form.Control as='select' type='text' className='formulario' placeholder="district" custom value={this.state.district_name} onChange={(event)=>(this.setState({...this.state, district_name:event.target.value}))} >
-                                    {this.state.district_name.map(d=>
-                                    <option>{[d]}</option>
-                                    )}</Form.Control>
+                                <Form.Control as='select'  type='text' className='formulario'  value={this.state.district_name} onChange={(event)=>(this.setState({district_name:event.target.value}))} >
+                                    <option disabled selected value=''>Distrito</option>
+                                    {this.state.district_names.map(d=>{
+                                    return <option key={d} value={d}>{[d]}</option>
+                                })}</Form.Control>
+                            </Col>
+                        </Form.Group>
+                        
+                        <Form.Group  as={Row}>
+                            <Col>
+                                ¿Cuantos metros² quieres?
+                            </Col> 
+                            <Col>
+                                <Form.Control as='select' type="number" className='formulario' placeholder="sup" value={this.state.sup} onChange={(event)=>this.setState({sup: event.target.type === 'number' ? parseInt(event.target.value) : event.target.value})}>
+                                <option disabled selected value=''>Metros²</option>
+                                {this.state.sup_list.map(s=>
+                                        <option>{[s]}</option>
+                                        )}</Form.Control>
                             </Col>
                         </Form.Group>
                         <Form.Group as={Row}>
@@ -48,15 +66,11 @@ class Research extends Component {
                                 ¿Cuantos baños necesitas?
                             </Col> 
                             <Col>
-                                <Form.Control type="number" className='formulario' placeholder="baths" value={this.state.baths} onChange={(event)=>this.setState({baths: event.target.type === 'number' ? parseInt(event.target.value) : event.target.value})} />
-                            </Col>
-                        </Form.Group>
-                        <Form.Group  as={Row}>
-                            <Col>
-                                ¿Cuantos metros² quieres?
-                            </Col> 
-                            <Col>
-                                <Form.Control type="number" className='formulario' placeholder="sup" value={this.state.sup} onChange={(event)=>this.setState({sup: event.target.type === 'number' ? parseInt(event.target.value) : event.target.value})} />
+                                <Form.Control as='select' type="number" className='formulario' placeholder="baths" value={this.state.baths} onChange={(event)=>this.setState({baths: event.target.type === 'number' ? parseInt(event.target.value) : event.target.value})}>
+                                <option disabled selected value=''>Baños</option>
+                                    {this.state.bath_list.map(b=>
+                                        <option>{[b]}</option>
+                                        )}</Form.Control>
                             </Col>
                         </Form.Group>
                         <Form.Group  as={Row}>
@@ -64,7 +78,11 @@ class Research extends Component {
                             ¿Cuantas habitaciones necesitas?
                             </Col> 
                             <Col>
-                                <Form.Control type="number" className='formulario' placeholder="rooms" value={this.state.rooms} onChange={(event)=>this.setState({rooms: event.target.type === 'number' ? parseInt(event.target.value) : event.target.value})} />
+                                <Form.Control as='select' type="number" className='formulario' placeholder="rooms" value={this.state.rooms} onChange={(event)=>this.setState({rooms: event.target.type === 'number' ? parseInt(event.target.value) : event.target.value})}>
+                                <option disabled selected value=''>Habitaciones</option>
+                                {this.state.room_list.map(r=>
+                                        <option>{[r]}</option>
+                                        )}</Form.Control>
                             </Col>
                         </Form.Group>
                         <Button onClick={this.handleOnClickSubmit}>Submit</Button>
