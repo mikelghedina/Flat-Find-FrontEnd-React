6b044289a9107ac72c2179ded7c7c5aea6483e01
mikelghedina <mikel.ghedina@gmail.com>
2021-04-01 13:37:40 +0200

    images and contact us

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,6 +1,10 @@
 import React from "react";
 import {Button, Card, Row} from "react-bootstrap";
 import './AboutUs.css'
+import gemma from '../../profileImages/Gemma.jpg'
+import edgar from '../../profileImages/Edgar.jpeg'
+import miguel from '../../profileImages/Miguel.jpeg'
+import mikel from '../../profileImages/Mikel.jpeg'
 
 
 const AboutUs =()=>(
@@ -8,40 +12,49 @@ const AboutUs =()=>(
         <Row className='row'>
             <div className='column'>
                 <Card className='card'>
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Img className='img' variant="top" src={gemma} />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
+                        <Card.Title>Gemma Reverter</Card.Title>
                         <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                            FullStack Developer
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary" href='https://www.linkedin.com/in/gemmareverter/'>My Linkedin</Button>
                     </Card.Body>
                 </Card>
             </div>
             <div className='column'>
                 <Card className='card'>
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Img className='img' variant="top"  src={mikel} />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
+                        <Card.Title>Miguel A. Arregui</Card.Title>
                         <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                            FullStack Developer
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary" href='https://www.linkedin.com/in/mikelghedina/'>My Linkedin</Button>
                     </Card.Body>
                 </Card>
             </div>
             <div className='column'>
                 <Card className='card'>
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Img className='img' variant="top"  src={edgar} />
                     <Card.Body>
-                        <Card.Title>Card Title</Card.Title>
+                        <Card.Title>Edgar Rodriguez</Card.Title>
                         <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
+                            Back-End Developer
                         </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
+                        <Button variant="primary" href='https://www.linkedin.com/in/edgarrodriguezdelacalle/'>My Linkedin</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className='column'>
+                <Card className='card'>
+                    <Card.Img className='img' variant="top"  src={miguel} />
+                    <Card.Body>
+                        <Card.Title>Miguel Romera</Card.Title>
+                        <Card.Text>
+                            Statistical
+                        </Card.Text>
+                        <Button variant="primary" href='https://www.linkedin.com/in/miguelromeraalegre/'>My Linkedin</Button>
                     </Card.Body>
                 </Card>
             </div>
