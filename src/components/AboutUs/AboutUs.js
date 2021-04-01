c387de6a02a8a1ebeb270f1968c4882d27cb2d43
mikelghedina <mikel.ghedina@gmail.com>
2021-04-01 11:08:39 +0200

    init aboutus styling cards and rows

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,9 +1,51 @@
 import React from "react";
+import {Button, Card, Row} from "react-bootstrap";
+import './AboutUs.css'
 
 
 const AboutUs =()=>(
     <div>
-        <h1>ABOUT US</h1>
+        <Row className='row'>
+            <div className='column'>
+                <Card className='card'>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className='column'>
+                <Card className='card'>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div className='column'>
+                <Card className='card'>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+        </Row>
     </div>
 
 );
